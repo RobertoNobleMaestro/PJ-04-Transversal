e40982bd426083fb938e13369562add7e9f52c74
kilianruiz <451093.joan23@fje.edu>
2025-03-20 17:54:27 +0100

    Pagina gimcana//mapas y control de sesiones

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::middleware(['auth'])->group(function () {
+    Route::get('/mapa', [AuthController::class, 'goMapa'])->name('mapa');
+    Route::get('/gimcana', [AuthController::class, 'goGimcana'])->name('gimcana');
+
 });
 Route::get('/', [AuthController::class, 'showHome']); 
 
@@ -15,3 +18,4 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
+Route::get('/index', [AuthController::class, 'showHome'])->name('index');
