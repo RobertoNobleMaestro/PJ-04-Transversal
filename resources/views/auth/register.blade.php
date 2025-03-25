18ba326be118814d4e14fbfafc3c33fd27db59e5
Mario Palamari <mariopalamari@gmail.com>
2025-03-25 16:22:48 +0100

    responsive

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top">
