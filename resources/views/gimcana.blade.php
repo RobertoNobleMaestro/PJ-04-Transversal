ecd0c54d6ff086ef90d15516eb57fb4d784b9694
Mario Palamari <mariopalamari@gmail.com>
2025-03-25 19:36:45 +0100

    complete merge

--- resources/views/gimcana.blade.php
+++ resources/views/gimcana.blade.php
@@ -14,7 +14,6 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.min.css" rel="stylesheet">
     {{-- link estilos css --}}
     <link href="{{ asset('css/clientegimcana.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/gimcana/datos.js') }}"></script>
     {{-- SweetAlert --}}
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
